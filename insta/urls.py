9458f241f99ba99985a59a0c3718d5bde662d64d
kangalah <nyongesajoannanjala@gmail.com>
2020-08-10 20:08:29 +0300

    add profile url

--- insta/urls.py
+++ insta/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.conf import settings
@@ -22,6 +23,11 @@ from user import views as user_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^register/', user_views.register, name='register'),
+    url(r'^profile/', user_views.profile, name='profile'),
+    url(r'^login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    url(r'^logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     url(r'', include('gram.urls')),
     
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
