2ccbce7ad9cc5f131059cde29f213d929c901eb7
kangalah <nyongesajoannanjala@gmail.com>
2020-08-12 14:09:34 +0300

    deploy

--- insta/settings.py
+++ insta/settings.py
@@ -164,11 +164,7 @@ LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
 
 # Email configurations remember to install python-decouple
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
