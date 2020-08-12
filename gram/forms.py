00695dffd14d2641a063662a3a43dad57596d39c
kangalah <nyongesajoannanjala@gmail.com>
2020-08-12 09:09:45 +0300

    delete the like form

--- gram/forms.py
+++ gram/forms.py
@@ -1,16 +1,13 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Post,Image,Comment
+from .models import Post,Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('image', 'caption')
 
-class ImageForm(forms.ModelForm):
-    class Meta:
-        model = Image
-        exclude =['likes','profile']
+
 
 
 class CommentForm(forms.ModelForm):
